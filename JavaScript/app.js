f70fa900e7acedd29a97250f10a407dce599652e
sahinmelikov <1rima621melikov@gmail.com>
2023-03-04 23:58:14 +0400

    Javascript Task

---
+++ JavaScript/app.js
@@ -0,0 +1,36 @@
+////1 ci tapsiriq
+
+let num,a
+num=890
+a=parseInt((num/100))%10
+console.log(a);
+
+
+
+////2 ci tapsiriq
+
+
+// let A,B,C
+// A=18;
+// B=23;
+// C=23;
+// if (A>=B && A>=C ) {
+//     console.log(A)
+//   }
+//   if (B>=A && B>=C  ) {
+//     console.log(B)
+//   }
+//   if (C>=A && C>=B) {
+//     console.log(C)
+    
+//   }
+//   if (A==B && A==C && B==C ) {
+//     console.log("3 deyisen bir birine beraberdir")
+    
+//   }
+
+
+
+
+        
+
